46b71e100b7a88e8b082567a8a87afe908ab462f
Christopher Arnold <chrisarnold2010@gmail.com>
2018-06-04 23:13:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay(musicians,instruments) {
   var newArray = [];
-  for (var i=0; i < 3; i++) {
+  for (var i=0; i < musicians.length; i++) {
     newArray.push(musicians[i] + " plays " + instruments[i]);
   }
   return newArray;
