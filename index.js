7c1dce96f741b5ee7b69d9641ebf9ca87cfe377b
Christopher Arnold <chrisarnold2010@gmail.com>
2018-06-04 23:06:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo St
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay(musicians,instruments) {
-  var 
+  var newArray = [];
+  for (i=-1; i < 3; i++) {
+    newArray.push(musicians[i] + " plays " + instruments[i]);
+  }
 }
